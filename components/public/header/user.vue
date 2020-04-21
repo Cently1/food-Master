5e0343621d8950af40c98c8b84c3cf2d036c5b53
Cently1 <1511384361@qq.com>
2020-04-21 23:10:44 +0800

    添加用户图片

--- components/public/header/user.vue
+++ components/public/header/user.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="m-user">
     <template v-if="user">
-      <span class="username">欢迎您,{{ user }}</span> <nuxt-link to="/exit"
-      >&nbsp;&nbsp;&nbsp;[退出]</nuxt-link
-      >
-      <img src="" alt="">
+      <span class="username">欢迎您,{{ user }}</span>
+      <nuxt-link to="/exit">&nbsp;&nbsp;&nbsp;[退出]</nuxt-link>
+      <div
+        ><img
+          src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
+          alt="用户头像"
+          class="icon"
+      /></div>
     </template>
     <template v-else>
       <nuxt-link to="/login" class="login">立即登录 </nuxt-link>
